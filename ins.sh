cf54c88d7d787cdc2aa7bb6a1ce4a86d7fe50cee
Nazar8 <118745073+88n77@users.noreply.github.com>
2025-11-08 12:16:23 +0000

    up

---
+++ ins.sh
@@ -0,0 +1,11 @@
+<p align="center">
+  <img src="https://raw.githubusercontent.com/88n77/Logo-88n77/main/logo.png" width="260" alt="88n77 Logo">
+</p>
+
+<h1 align="center"> 88n77 NODES</h1>
+<p align="center">
+  Independent Web3 Infrastructure & Automation Lab<br>
+  <a href="https://x.com/88n77n">Twitter</a> • 
+  <a href="https://t.me/88n77crypt">Telegram</a> • 
+  <a href="https://88n77.com.ua">Website</a>
+</p>
