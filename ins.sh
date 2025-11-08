4b129854ef03f9799f93153feff0e62e6f853f20
Nazar8 <118745073+88n77@users.noreply.github.com>
2025-11-08 12:19:38 +0000

    up

--- ins.sh
+++ ins.sh
@@ -1,5 +1,5 @@
 <p align="center">
-  <img src="https://raw.githubusercontent.com/88n77/Logo-88n77/main/logo.png" width="260" alt="88n77 Logo">
+  <img src="https://raw.githubusercontent.com/88n77/Logo-88n77/main/logo.sh" width="260" alt="88n77 Logo">
 </p>
 
 <h1 align="center"> 88n77 NODES</h1>
